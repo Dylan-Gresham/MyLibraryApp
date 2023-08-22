6fcfeefa4b2c7a9ed808b3cacd00a6efe29b1b89
Dylan-Gresham <dgresh724@gmail.com>
2023-08-22 14:15:38 -0600

    Add method stubs to book.c

--- book.c
+++ book.c
@@ -1,3 +1,53 @@
 //
 // Created by Dylan Gresham on 08/22/2023
 //
+
+#include "./book.h"
+
+Book * CreateBook(char title[], char author[], int numPages, int rating, bool completed) {
+    // TODO
+}
+
+int DestroyBook(Book * book) {
+    // TODO
+}
+
+char * GetTitle(Book * book) {
+    // TODO
+}
+
+char * GetAuthor(Book * book) {
+    // TODO
+}
+
+int GetNumPages(Book * book) {
+    // TODO
+}
+
+int GetRating(Book * book) {
+    // TODO
+}
+
+bool GetReadingStatus(Book * book) {
+    // TODO
+}
+
+int SetTitle(Book * book, char * newTitle) {
+    // TODO
+}
+
+int SetAuthor(Book * book, char * newAuthor) {
+    // TODO
+}
+
+int SetNumPages(Book * book, int newNumPages) {
+    // TODO
+}
+
+int SetRating(Book * book, int newRating) {
+    // TODO
+}
+
+int SetReadingStatus(Book * book, bool newStatus) {
+    // TODO
+}
