d0a67e55670c48ed1d5641839cfe35841b1fffaf
Dylan-Gresham <dgresh724@gmail.com>
2023-08-24 13:45:40 -0600

    Create method stubs in library.c

--- library.c
+++ library.c
@@ -1,3 +1,37 @@
 //
 // Created by Dylan Gresham on 08/22/2023
 //
+
+#include "library.h"
+
+Library *CreateLibrary(int initialSize) {
+    // TODO
+}
+
+int DestroyLibrary(Library *library) {
+    // TODO
+}
+
+int AddBook(Library *library, Book *book) {
+    // TODO
+}
+
+Book **GetBooks(Library *library) {
+    // TODO
+}
+
+int GetNumBooks(Library *library) {
+    // TODO
+}
+
+int GetNumAuthors(Library *library) {
+    // TODO
+}
+
+int SetNumAuthors(Library *library, int newNumAuthors) {
+    // TODO
+}
+
+bool IsEmpty(Library *library) {
+    // TODO
+}
