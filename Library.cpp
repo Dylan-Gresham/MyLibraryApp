8fc6f4988ce67da45ba02373f480de708bfbd83b
Dylan-Gresham <dgresh724@gmail.com>
2023-08-26 22:56:17 -0600

    Delete original C files & finish C++ conversion

---
+++ Library.cpp
@@ -0,0 +1,49 @@
+//
+// Library.cpp
+// Created by Dylan Gresham on 8/26/23.
+//
+
+#include "Library.h"
+using namespace std;
+
+Library::Library(int initialCapacity) {
+    this->numBooks = 0;
+    this->numAuthors = 0;
+    // Don't need to initialize booksVector or authorsMap as they are created as empty
+    // when the constructor is called
+}
+
+void Library::addBook(std::string title, std::string author, int numPages, int rating, int status) {
+    Book newBook = new Book(title, author, numPages, rating, status);
+    this->booksVector.push_back(newBook);
+    this->numBooks = this->booksVector.size();
+    /*
+     *  According to
+     *  https://stackoverflow.com/questions/72841438/how-to-store-array-data-as-map-key-or-increment-frequency-if-array-already-in-ma
+     *  if you just use the increment operator on a map key it'll increment it if it's in the map
+     *  and if it isn't in the map it adds it with default value of 0 and then increments it
+     *  to 1 which is exactly what I want to do. TODO: Will need to test this.
+     */
+    this->authorsMap[title]++;
+    this->numAuthors = this->authorsMap.size();
+}
+
+std::vector<Book> Library::getBooks() {
+    return this->booksVector;
+}
+
+std::map<string, int> Library::getAuthors() {
+    return this->authorsMap;
+}
+
+int Library::getNumBooks() {
+    return this->numBooks;
+}
+
+int Library::getNumAuthors() {
+    return this->numAuthors;
+}
+
+bool Library::isEmpty() {
+    return (this->booksVector.size() == 0);
+}
---
+++ Library.h
@@ -0,0 +1,81 @@
+//
+// Library.h
+// Created by Dylan Gresham on 8/26/23.
+//
+
+#ifndef MYLIBRARYAPP_LIBRARY_H
+#define MYLIBRARYAPP_LIBRARY_H
+
+#include "./Book.h"
+
+#include <vector>
+#include <map>
+using namespace std;
+
+class Library {
+    private:
+        int numBooks;
+        int numAuthors;
+        std::vector<Book> booksVector;
+        std::map<string, int> authorsMap;
+    public:
+        /**
+         * Creates a new Library object with the specified initial capacity for books.
+         *
+         * @param initialSize - The initial capacity of the library
+         *
+         * @return A newly created Library object
+         */
+        Library(int initialCapacity);
+
+        /**
+         * Adds the specified book to the library.
+         *
+         * @param title A string containing the title of the book to be added
+         * @param author A string containing the author of the book to be added
+         * @param numPages An int representing the total number of pages in the book
+         * @param rating An int representing the rating of the book (-1 [negative one] if you don't want to give a rating yet).
+         * @param status An int representing your reading status of the book. 0 = planned, 1 = reading, 2 = dropped, 3 = aside, 4 = completed
+         *
+         * @return 0 on success, nonzero on error
+         */
+        void addBook(string title, string author, int numPages, int rating, int status);
+
+        /**
+         * Gets the list of books in the library.
+         *
+         * @return A vector containing all the books currently in the library.
+         */
+        std::vector<Book> getBooks();
+
+        /**
+         * Gets the list of authors in the library.
+         *
+         * @return A vector containing all the authors currently in the library.
+         */
+        std::map<string, int> getAuthors();
+
+        /**
+         * Gets the total number of books in the library.
+         *
+         * @return An integer (int) representing the total number of books in the library, nonzero on error
+         */
+        int getNumBooks();
+
+        /**
+         * Gets the total number of authors in the library.
+         *
+         * @return An integer (int) representing the total number of authors in the library, nonzero on error
+        */
+        int getNumAuthors();
+
+        /**
+         * Checks if the library is empty or not
+         *
+         * @return True if the library is empty, false if non-empty.
+        */
+        bool isEmpty();
+};
+
+
+#endif //MYLIBRARYAPP_LIBRARY_H
